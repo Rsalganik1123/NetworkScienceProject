de7b3e3724ba64e7c22ed1b411864cebc2f46fef
Rebecca Salganik <rebeccasalganik@Rebeccas-MacBook-Pro.local>
2021-10-27 11:40:40 -0400

    catcher in post_process

--- src/scraping/lyric_loader.py
+++ src/scraping/lyric_loader.py
@@ -10,21 +10,9 @@ from tqdm import tqdm
 import os
 from lyricsgenius import Genius
 from requests.exceptions import HTTPError, Timeout
-
+from src.utils.utils import chunks
 token = 'XxjduohJNSubKbqL47-dEAO6nSDtWgawot7hwF5qyyemBKT7yb0EwIOiqHt84SSC'
 genius = Genius(token)
-
-
-class SongNotInDB(Exception):
-    def __init__(self, message="Song not found in Genius DB"):
-        self.message = message
-        super().__init__(self.message)
-
-
-class NotLyrical(Exception):
-    def __init__(self, message="Song doesn't have lyrics"):
-        self.message = message
-        super().__init__(self.message)
 
 class WrongEntry(Exception): 
     def __init__(self, message="Wrong song entry found"):
@@ -35,10 +23,10 @@ class Lyric_Loader():
     def __init__(self):
         self.track_count = 0
         self.skipped_tracks = 0
-        self.lyric_fields = {"arid", "track", "artist", "lyrics"}
+        self.lyric_fields = {"arid", "track", "artist","gen_t", "gen_a", "correct_lyric_scrape", "lyrics"}
         self.lyrics = []
         self.skipped_value_names = []
-
+        self.potentially_wrong_entries = [] 
     def process(self, batch):
         for arid, track, artist in batch:
             try:
@@ -49,13 +37,13 @@ class Lyric_Loader():
                 correct_lyric_scrape = gen_a.lower() == artist.lower()
                 print("GENIUS T: {}, GENIUS A: {}, CORRECT?: {}".format(song.title, gen_a, correct_lyric_scrape))
                 if not correct_lyric_scrape: 
-                    raise WrongEntry()
-                
+                    self.potentially_wrong_entries.append((arid, gen_t, gen_a))
+                #     raise WrongEntry()
                 # lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
                 # regex = re.compile('[^a-zA-Z \n]')
                 # lyrics = regex.sub('', lyrics)
                 #Preprocess here (before you store) with: self.preprocess_lyrics(lyrics)
-                self.store_lyrics(arid, track, artist, lyrics)
+                self.store_lyrics(arid, track, artist, gen_t, gen_a, correct_lyric_scrape, lyrics)
                 self.track_count += 1
             except Exception as e:
                 print("E: {}, A: {}, T: {}".format(e, artist, track))
@@ -70,8 +58,8 @@ class Lyric_Loader():
         track = track.lower()
         return track, artist
 
-    def store_lyrics(self, arid, track, artist, lyrics):
-        data = [arid, track, artist, lyrics]
+    def store_lyrics(self, arid, track, artist, gen_t, gen_a, correct_lyric_scrape, lyrics):
+        data = [arid, track, artist, gen_t, gen_a, correct_lyric_scrape, lyrics]
         self.lyrics.append(data)
 
     def preprocess_lyrics(self, lyrics): 
@@ -88,18 +76,15 @@ class Lyric_Loader():
         with open(path_save + "missed_lyric_scape_vals.txt", "a") as f:
             for v in self.skipped_value_names:
                 f.write(' '.join(str(s) for s in v) + '\n')
+        with open(path_save + "potentially_wrong_lyrics.txt", "a") as f:
+            for v in self.potentially_wrong_entries:
+                f.write(' '.join(str(s) for s in v) + '\n')
 
     def show_summary(self):
         print("**** Loaded: {} tracks ****".format(self.track_count))
         print("**** Skipped {} tracks ****".format(self.skipped_tracks))
 
-
-def chunks(l, n):
-    for i in range(0, len(l), n):
-        yield l[i:i + n]
-
-
-def load_batches(track_path, artist_path):
+def load_batches(track_path, artist_path, write_path):
     track_df = pd.read_csv(track_path, sep='\t')
     artist_df = pd.read_csv(artist_path, sep='\t')
     joined_df = track_df.join(artist_df.set_index('arid'), on='arid')
@@ -112,37 +97,33 @@ def load_batches(track_path, artist_path):
     init = True
     for batch in tqdm(batches):
         l.process(batch)
-        # l.writer('/Users/rebeccasalganik/Documents/School/2021-2022/Network Science/Capstone/spotify_in_csv/', init)
+        # l.writer(write_path, init)
         init = False
 
-track_path, artist_path = "/Users/rebeccasalganik/Documents/School/2021-2022/Network Science/Capstone/spotify_in_csv/tracks.csv", "/Users/rebeccasalganik/Documents/School/2021-2022/Network Science/Capstone/spotify_in_csv/artists.csv"
+path = '/Users/rebeccasalganik/Documents/School/2021-2022/Network Science/Capstone/'
+track_path, artist_path, write_path = path +"/spotify_in_csv/tracks.csv", path + "/spotify_in_csv/artists.csv", path + "spotify_in_csv/"
 
-# load_batches(track_path, artist_path)
+# load_batches(track_path, artist_path, write_path)
 
 
+def tester(): 
+    l = Lyric_Loader()
+    tid, track, artist = 1, 'Symphony No. 7', 'Ludwig Van Beethoven'
+    batch = [(tid, track, artist)]
+    l.process(batch)
 
-# l = Lyric_Loader()
-# tid, track, artist = 1, 'Symphony No. 7', 'Ludwig Van Beethoven'
-# batch = [(tid, track, artist)]
-# l.process(batch)
+    try:
+        track, artist = 'Symphony No. 7', 'Ludwig Van Beethoven'
+        artist = genius.search_artist(artist, max_songs=1, sort="title")
+        print(artist)
+        lyrics = artist.song(track).lyrics
+        print(lyrics[:50])
+        
+        # song = genius.search_song(track, artist)
 
-# try:
-#     track, artist = 'Symphony No. 7', 'Ludwig Van Beethoven'
-#     artist = genius.search_artist(artist, max_songs=1, sort="title")
-#     print(artist)
-#     lyrics = artist.song(track).lyrics
-#     print(lyrics[:50])
-    
-#     # song = genius.search_song(track, artist)
+    except Exception as e:
+        print(e)
+        print("MESSAGE", e.errno)  # status code
+        print(e.args[0])  # status code
+        print(e.args[1])  # error message
 
-# except Exception as e:
-#     print(e)
-#     print("MESSAGE", e.errno)  # status code
-#     print(e.args[0])  # status code
-#     print(e.args[1])  # error message
-# except HTTPError as e:
-#     print(e.errno)    # status code
-#     print(e.args[0])  # status code
-#     print(e.args[1])  # error message
-# except Timeout:
-#     pass
